b05288c0a4216d0da2fc9fa8462f9d27fcade4fd
shubh1855 <paulshubhranil1855@gmail.com>
2026-05-24 11:05:11 +0530

    feat(db): integrate PostgreSQL with sqlc-generated query layer

--- internal/cli/state.go
+++ internal/cli/state.go
@@ -1,11 +1,18 @@
 package cli
 
-import "github.com/shubh1855/Gator/internal/config"
+import (
+	"github.com/shubh1855/Gator/internal/config"
+	"github.com/shubh1855/Gator/internal/database"
+)
 
 type State struct {
 	Config *config.Config
+	DB     *database.Queries
 }
 
-func NewState(cfg *config.Config) *State {
-	return &State{Config: cfg}
+func NewState(cfg *config.Config, db *database.Queries) *State {
+	return &State{
+		Config: cfg,
+		DB:     db,
+	}
 }
--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"os"
 
+	_ "github.com/lib/pq"
+
 	"github.com/shubh1855/Gator/internal/cli"
 	"github.com/shubh1855/Gator/internal/config"
+	"github.com/shubh1855/Gator/internal/database"
 )
 
 func main() {
@@ -18,7 +22,19 @@ func main() {
 		log.Fatal(err)
 	}
 
-	s := cli.NewState(&cfg)
+	db, err := sql.Open("postgres", cfg.DBURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	if err := db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+
+	dbQueries := database.New(db)
+
+	state := cli.NewState(&cfg, dbQueries)
 
 	cmds := cli.NewCommands()
 	cmds.Register("login", cli.HandlerLogin)
@@ -28,7 +44,7 @@ func main() {
 		Args: os.Args[2:],
 	}
 
-	if err := cmds.Run(s, cmd); err != nil {
+	if err := cmds.Run(state, cmd); err != nil {
 		log.Fatal(err)
 	}
 }
